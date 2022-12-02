b34bc3b53ef885ee955da3881b3c26ddd3bed3f4
Louis Freret <Loukis95@gmail.com>
2022-12-02 08:04:40 +0100

    Part 2

--- aoc_02_2/src/main.rs
+++ aoc_02_2/src/main.rs
@@ -1,8 +1,122 @@
 use std::{env, fs};
 
-enum Values<T> {
-    Skip,
-    Value(T)
+fn score_for_when_rock(r2: &str) -> usize {
+    let r2_rock = r2.cmp("A");
+    let r2_paper = r2.cmp("B");
+    let r2_scissors = r2.cmp("C");
+
+    match r2_rock {
+        std::cmp::Ordering::Equal => 1+3,
+        _ => match r2_paper {
+            std::cmp::Ordering::Equal => 2+6,
+            _ => match r2_scissors {
+                std::cmp::Ordering::Equal => 3+0,
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn score_for_when_paper(r2: &str) -> usize {
+    let r2_rock = r2.cmp("A");
+    let r2_paper = r2.cmp("B");
+    let r2_scissors = r2.cmp("C");
+
+    match r2_rock {
+        std::cmp::Ordering::Equal => 1+0,
+        _ => match r2_paper {
+            std::cmp::Ordering::Equal => 2+3,
+            _ => match r2_scissors {
+                std::cmp::Ordering::Equal => 3+6,
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn score_for_when_scissors(r2: &str) -> usize {
+    let r2_rock = r2.cmp("A");
+    let r2_paper = r2.cmp("B");
+    let r2_scissors = r2.cmp("C");
+
+    match r2_rock {
+        std::cmp::Ordering::Equal => 1+6,
+        _ => match r2_paper {
+            std::cmp::Ordering::Equal => 2+0,
+            _ => match r2_scissors {
+                std::cmp::Ordering::Equal => 3+3,
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn response_for_when_rock(r2: &str) -> &'static str {
+    let r2_lose = r2.cmp("X");
+    let r2_draw = r2.cmp("Y");
+    let r2_win = r2.cmp("Z");
+
+    match r2_lose {
+        std::cmp::Ordering::Equal => "C",
+        _ => match r2_draw {
+            std::cmp::Ordering::Equal => "A",
+            _ => match r2_win {
+                std::cmp::Ordering::Equal => "B",
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn response_for_when_paper(r2: &str) -> &'static str {
+    let r2_lose = r2.cmp("X");
+    let r2_draw = r2.cmp("Y");
+    let r2_win = r2.cmp("Z");
+
+    match r2_lose {
+        std::cmp::Ordering::Equal => "A",
+        _ => match r2_draw {
+            std::cmp::Ordering::Equal => "B",
+            _ => match r2_win {
+                std::cmp::Ordering::Equal => "C",
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn response_for_when_scissors(r2: &str) -> &'static str {
+    let r2_lose = r2.cmp("X");
+    let r2_draw = r2.cmp("Y");
+    let r2_win = r2.cmp("Z");
+
+    match r2_lose {
+        std::cmp::Ordering::Equal => "B",
+        _ => match r2_draw {
+            std::cmp::Ordering::Equal => "C",
+            _ => match r2_win {
+                std::cmp::Ordering::Equal => "A",
+                _ => { println!("ERROR2"); 0 }
+            }
+        }
+    }
+}
+
+fn round_score(r1: &str, r2: &str) -> usize {
+    let r1_rock = r1.cmp("A");
+    let r1_paper = r1.cmp("B");
+    let r1_scissors = r1.cmp("C");
+
+    match r1_rock {
+        std::cmp::Ordering::Equal => score_for_when_rock(response_for_when_rock(r2)),
+        _ => match r1_paper {
+            std::cmp::Ordering::Equal => score_for_when_paper(response_for_when_paper(r2)),
+            _ => match r1_scissors {
+                std::cmp::Ordering::Equal => score_for_when_scissors(response_for_when_scissors(r2)),
+                _ => { println!("ERROR1"); 0 }
+            }
+        }
+    }
 }
 
 fn main() {
@@ -12,33 +126,14 @@ fn main() {
     let raw_string = String::from_utf8_lossy(&raw_input);
     let input : Vec<_> = raw_string.lines().collect();
 
-    let mut max_values = input.iter()
-                        .scan(0, |state: &mut usize, &x| {
-                            if x.is_empty() {
-                                let ret = Some(Values::Value(*state));
-                                *state = 0;
-                                ret
-                            } else {
-                                *state += x.parse::<usize>().unwrap();
-                                Some(Values::Skip)
-                            }
+    let total_score: usize = input.iter()
+                        .map(|line| {
+                            let mut it = line.split_whitespace();
+                            let r1 = it.next().unwrap();
+                            let r2 = it.next().unwrap();
+                            round_score(r1, r2)
                         })
-                        .filter_map(|x| {
-                            match x {
-                                Values::Skip => None,
-                                Values::Value(v) => Some(v),
-                            }
-                        })
-                        .collect::<Vec<_>>();
-
-    max_values.sort_unstable();
-    
-    println!("Sorted: {}, {}, {}", max_values[0], max_values[1], max_values[2]);
-
-    let sum_of_top_three: usize = max_values.iter()
-                        .rev()
-                        .take(3)
                         .sum();
 
-    println!("Value: {}", sum_of_top_three);
+    println!("Total score: {}", total_score);
 }

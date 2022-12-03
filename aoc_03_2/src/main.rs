e5b81f811ac1d63e31bc745358942fdfcff1e6df
Louis Freret <Loukis95@gmail.com>
2022-12-03 09:20:09 +0100

    Part 2

--- aoc_03_2/src/main.rs
+++ aoc_03_2/src/main.rs
@@ -1,121 +1,60 @@
 use std::{env, fs};
 
-fn score_for_when_rock(r2: &str) -> usize {
-    let r2_rock = r2.cmp("A");
-    let r2_paper = r2.cmp("B");
-    let r2_scissors = r2.cmp("C");
-
-    match r2_rock {
-        std::cmp::Ordering::Equal => 1+3,
-        _ => match r2_paper {
-            std::cmp::Ordering::Equal => 2+6,
-            _ => match r2_scissors {
-                std::cmp::Ordering::Equal => 3+0,
-                _ => { println!("ERROR2"); 0 }
-            }
-        }
-    }
-}
-
-fn score_for_when_paper(r2: &str) -> usize {
-    let r2_rock = r2.cmp("A");
-    let r2_paper = r2.cmp("B");
-    let r2_scissors = r2.cmp("C");
-
-    match r2_rock {
-        std::cmp::Ordering::Equal => 1+0,
-        _ => match r2_paper {
-            std::cmp::Ordering::Equal => 2+3,
-            _ => match r2_scissors {
-                std::cmp::Ordering::Equal => 3+6,
-                _ => { println!("ERROR2"); 0 }
-            }
-        }
-    }
-}
-
-fn score_for_when_scissors(r2: &str) -> usize {
-    let r2_rock = r2.cmp("A");
-    let r2_paper = r2.cmp("B");
-    let r2_scissors = r2.cmp("C");
-
-    match r2_rock {
-        std::cmp::Ordering::Equal => 1+6,
-        _ => match r2_paper {
-            std::cmp::Ordering::Equal => 2+0,
-            _ => match r2_scissors {
-                std::cmp::Ordering::Equal => 3+3,
-                _ => { println!("ERROR2"); 0 }
-            }
-        }
-    }
-}
-
-fn response_for_when_rock(r2: &str) -> &'static str {
-    let r2_lose = r2.cmp("X");
-    let r2_draw = r2.cmp("Y");
-    let r2_win = r2.cmp("Z");
-
-    match r2_lose {
-        std::cmp::Ordering::Equal => "C",
-        _ => match r2_draw {
-            std::cmp::Ordering::Equal => "A",
-            _ => match r2_win {
-                std::cmp::Ordering::Equal => "B",
-                _ => { println!("ERROR2"); "N" }
-            }
-        }
-    }
-}
-
-fn response_for_when_paper(r2: &str) -> &'static str {
-    let r2_lose = r2.cmp("X");
-    let r2_draw = r2.cmp("Y");
-    let r2_win = r2.cmp("Z");
-
-    match r2_lose {
-        std::cmp::Ordering::Equal => "A",
-        _ => match r2_draw {
-            std::cmp::Ordering::Equal => "B",
-            _ => match r2_win {
-                std::cmp::Ordering::Equal => "C",
-                _ => { println!("ERROR2"); "N" }
-            }
-        }
-    }
-}
-
-fn response_for_when_scissors(r2: &str) -> &'static str {
-    let r2_lose = r2.cmp("X");
-    let r2_draw = r2.cmp("Y");
-    let r2_win = r2.cmp("Z");
-
-    match r2_lose {
-        std::cmp::Ordering::Equal => "B",
-        _ => match r2_draw {
-            std::cmp::Ordering::Equal => "C",
-            _ => match r2_win {
-                std::cmp::Ordering::Equal => "A",
-                _ => { println!("ERROR2"); "N" }
-            }
-        }
-    }
-}
-
-fn round_score(r1: &str, r2: &str) -> usize {
-    let r1_rock = r1.cmp("A");
-    let r1_paper = r1.cmp("B");
-    let r1_scissors = r1.cmp("C");
-
-    match r1_rock {
-        std::cmp::Ordering::Equal => score_for_when_rock(response_for_when_rock(r2)),
-        _ => match r1_paper {
-            std::cmp::Ordering::Equal => score_for_when_paper(response_for_when_paper(r2)),
-            _ => match r1_scissors {
-                std::cmp::Ordering::Equal => score_for_when_scissors(response_for_when_scissors(r2)),
-                _ => { println!("ERROR1"); 0 }
-            }
-        }
+fn priority(c: &char) -> usize {
+    match c {
+        'a' => 1,
+        'b' => 2,
+        'c' => 3,
+        'd' => 4,
+        'e' => 5,
+        'f' => 6,
+        'g' => 7,
+        'h' => 8,
+        'i' => 9,
+        'j' => 10,
+        'k' => 11,
+        'l' => 12,
+        'm' => 13,
+        'n' => 14,
+        'o' => 15,
+        'p' => 16,
+        'q' => 17,
+        'r' => 18,
+        's' => 19,
+        't' => 20,
+        'u' => 21,
+        'v' => 22,
+        'w' => 23,
+        'x' => 24,
+        'y' => 25,
+        'z' => 26,
+        'A' => 27,
+        'B' => 28,
+        'C' => 29,
+        'D' => 30,
+        'E' => 31,
+        'F' => 32,
+        'G' => 33,
+        'H' => 34,
+        'I' => 35,
+        'J' => 36,
+        'K' => 37,
+        'L' => 38,
+        'M' => 39,
+        'N' => 40,
+        'O' => 41,
+        'P' => 42,
+        'Q' => 43,
+        'R' => 44,
+        'S' => 45,
+        'T' => 46,
+        'U' => 47,
+        'V' => 48,
+        'W' => 49,
+        'X' => 50,
+        'Y' => 51,
+        'Z' => 52,
+        _ => { println!("error"); 0 }
     }
 }
 
@@ -127,11 +66,31 @@ fn main() {
     let input : Vec<_> = raw_string.lines().collect();
 
     let total_score: usize = input.iter()
-                        .map(|line| {
-                            let mut it = line.split_whitespace();
-                            let r1 = it.next().unwrap();
-                            let r2 = it.next().unwrap();
-                            round_score(r1, r2)
+                        .zip(input.iter().skip(1))
+                        .zip(input.iter().skip(2))
+                        .step_by(3)
+                        .map(|((e1, e2), e3)| {
+                            println!("{} - {} - {}", e1, e2, e3);
+                            let mut v1 = e1.chars().collect::<Vec<_>>();
+                            let mut v2 = e2.chars().collect::<Vec<_>>();
+                            let mut v3 = e3.chars().collect::<Vec<_>>();
+                            v1.sort_unstable();
+                            v2.sort_unstable();
+                            v3.sort_unstable();
+                            v1.dedup();
+                            v2.dedup();
+                            v3.dedup();
+                            for c in v1.iter() {
+                                let r2 = v2.binary_search(c);
+                                if r2.is_ok() {
+                                    let r3 = v3.binary_search(c);
+                                    if r3.is_ok() {
+                                        return priority(c);
+                                    }
+                                }
+                            }
+                            println!("ERROR");
+                            0
                         })
                         .sum();
 

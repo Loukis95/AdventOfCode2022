66b7024ea5eb1e541c1e8324ebe34ad4804c9e62
Louis Freret <Loukis95@gmail.com>
2022-12-04 08:35:29 +0100

    Feature/aoc4 (#6)
    * Prepare aoc 04
    
    * AoC 04
    
    * Part 2

---
+++ aoc_04_2/src/main.rs
@@ -0,0 +1,20 @@
+use std::{env, fs};
+
+fn main() {
+    let args : Vec<_> = env::args().collect();
+    let input_path = &args[1];
+    let raw_input = fs::read(input_path).unwrap();
+    let raw_string = String::from_utf8_lossy(&raw_input);
+    let input : Vec<_> = raw_string.lines().collect();
+
+    let total_score: usize = input.iter()
+                                .filter(|line| {
+                                    let assignments = line.split(',').collect::<Vec<_>>();
+                                    let range1 = assignments[0].split('-').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+                                    let range2 = assignments[1].split('-').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+                                    (range1[0] >= range2[0] && range1[0] <= range2[1]) || (range1[1] >= range2[0] && range1[1] <= range2[1]) || (range1[0] >= range2[0] && range1[1] <= range2[1]) || (range2[0] >= range1[0] && range2[1] <= range1[1])
+                                })
+                                .count();
+
+    println!("Total score: {}", total_score);
+}

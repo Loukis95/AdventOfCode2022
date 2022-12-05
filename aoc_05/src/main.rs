a64ec877f3df8c93fc57f65502363780800b7ee5
Louis Freret <loukis95@gmail.com>
2022-12-05 08:38:19 +0100

    progress on aoc 05

--- aoc_05/src/main.rs
+++ aoc_05/src/main.rs
@@ -1,4 +1,13 @@
 use std::{env, fs};
+use regex::Regex;
+
+fn move_n_from_x_to_y(stacks: &mut Vec<Vec<char>>, n: usize, x: usize, y: usize) {
+    for _i in 0..n {
+        if let Some(c) = stacks[x].pop() {
+            stacks[y].push(c);
+        }
+    }
+}
 
 fn main() {
     let args : Vec<_> = env::args().collect();
@@ -7,13 +16,48 @@ fn main() {
     let raw_string = String::from_utf8_lossy(&raw_input);
     let input : Vec<_> = raw_string.lines().collect();
 
-    let total_score: usize = input.iter()
-                                .filter(|line| {
-                                    let assignments = line.split(',').collect::<Vec<_>>();
-                                    let range1 = assignments[0].split('-').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
-                                    let range2 = assignments[1].split('-').map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
-                                    (range1[0] >= range2[0] && range1[1] <= range2[1]) || (range2[0] >= range1[0] && range2[1] <= range1[1])
-                                }).count();
+    let (pos, _) = input.iter().enumerate().find(|(_, line)| line.is_empty()).unwrap();
+    let (stacks_str, program) = input.split_at(pos);
+    let nb_stacks = stacks_str.last().unwrap().split_whitespace().last().unwrap().parse::<usize>().unwrap();
 
-    println!("Total score: {}", total_score);
+    println!("nb_stacks {}", nb_stacks);
+
+    let mut stacks: Vec<Vec<char>> = vec![vec![]; nb_stacks];
+    stacks_str.iter()
+        .rev()
+        .skip(1)
+        .for_each(|line| {
+            line.chars()
+                .enumerate()
+                .filter(|(_, c)| *c != '[' && *c != ']' && *c != ' ')
+                .for_each(|(pos, c)| {
+                    let stack_nb = (pos-1)/4;
+                    println!("{}: {} in stack {}", pos, c, stack_nb);
+                    stacks[stack_nb].push(c);
+                });
+        });
+
+    println!("{:?}", stacks);
+    
+    program.iter().skip(1)
+        .for_each(|line| {
+            println!("{}", line);
+            let re = Regex::new(r"^move ([0-9]+) from ([0-9]+) to ([0-9]+)").unwrap();
+            re.captures_iter(line).
+            let pr = line.chars().
+            .map(|x| x.parse::<usize>().unwrap()).collect::<Vec<_>>();
+            println!("{:?}", pr);
+            let (n, from, to) = (pr[0], pr[1]-1, pr[2]-1);
+            move_n_from_x_to_y(&mut stacks, n, from, to);
+            println!("{:?}", stacks);
+        });
+        
+    print!("response: ");
+    for i in 0..nb_stacks {
+        if let Some(c) = stacks[i].last() {
+            print!("{}", c);
+        }
+    }
+    println!("");
+
 }

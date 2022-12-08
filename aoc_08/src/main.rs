2883f37a0ef73c08988ddb70a6c711a7241e8c10
Louis Freret <loukis95@gmail.com>
2022-12-08 08:00:17 +0100

    part 1

--- aoc_08/src/main.rs
+++ aoc_08/src/main.rs
@@ -46,59 +46,53 @@ fn main() {
     let n = forest[0].len();
     println!("Forest size: {}x{}", n, m);
 
-    let mut nb_hidden_trees : usize = m*4-4;
+    let mut nb_hidden_trees : usize = m*2+n*2-4;
 
     for j in 1..m-1 {
         for i in 1..n-1 {
             let height = forest[j][i];
-            let mut found = false;
+            let mut found = [false;4];
             let mut x = i;
             let mut y = j;
             while let Some((u, v)) = neighbour_left(x, y, n, m) {
-                if forest[v][u] > height {
-                    found = true;
+                if forest[v][u] >= height {
+                    found[0] = true;
                     break;
                 }
                 x = u;
                 y = v;
             }
-            if !found { continue; }
-            found = false;
             x = i;
             y = j;
             while let Some((u, v)) = neighbour_right(x, y, n, m) {
-                if forest[v][u] > height {
-                    found = true;
+                if forest[v][u] >= height {
+                    found[1] = true;
                     break;
                 }
                 x = u;
                 y = v;
             }
-            if !found { continue; }
-            found = false;
             x = i;
             y = j;
             while let Some((u, v)) = neighbour_up(x, y, n, m) {
-                if forest[v][u] > height {
-                    found = true;
+                if forest[v][u] >= height {
+                    found[2] = true;
                     break;
                 }
                 x = u;
                 y = v;
             }
-            if !found { continue; }
-            found = false;
             x = i;
             y = j;
             while let Some((u, v)) = neighbour_down(x, y, n, m) {
-                if forest[v][u] > height {
-                    found = true;
+                if forest[v][u] >= height {
+                    found[3] = true;
                     break;
                 }
                 x = u;
                 y = v;
             }
-            if found { nb_hidden_trees += 1; }
+            if !found[0] || !found[1] || !found[2] || !found[3] { nb_hidden_trees += 1; println!("({}, {})",i,j); }
         }
     }
 

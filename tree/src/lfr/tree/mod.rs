1a69f00c4ea934064e5467aafbea93bc13168eda
Louis Freret <Loukis95@gmail.com>
2022-12-10 23:13:10 +0100

    Tree crate

--- tree/src/lfr/tree/mod.rs
+++ tree/src/lfr/tree/mod.rs
@@ -1,9 +1,9 @@
 use std::collections::VecDeque;
+use std::fmt::Display;
 use std::marker::PhantomData;
 
 mod internal {
 
-    use std::fmt::Display;
     use std::rc::{Weak, Rc};
     use std::cell::RefCell;
 
@@ -39,11 +39,15 @@ mod internal {
         pub fn get_mut(&mut self) -> &mut T {
             &mut self.item
         }
-        
+
         pub fn push(&mut self, item: T) {
             let child = Self::new(item);
             child.borrow_mut().parent = self.this.clone();
             self.children.push(child);
+        }
+    
+        pub fn append(&mut self, node: RcTreeNode<T>) {
+            self.children.push(node)
         }
 
         pub fn parent(&self) -> WeakTreeNode<T> {
@@ -54,14 +58,6 @@ mod internal {
             &self.children
         }
 
-    }
-
-    impl<T> Display for TreeNode<T>
-    where T: Display
-    {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{}", self.item)
-        }
     }
 
 }
@@ -84,8 +80,12 @@ impl<T> Tree<T> {
         self.node.borrow_mut().push(item);
     }
     
+    pub fn append(&mut self, tree: Tree<T>) {
+        self.node.borrow_mut().append(tree.node);
+    } 
+    
     pub fn get(&self) -> &T {
-        let tmp: *const T = self.node.borrow().get();
+        let tmp: *const T = (*self.node).borrow().get();
         unsafe { tmp.as_ref().unwrap() }
     }
 
@@ -95,7 +95,7 @@ impl<T> Tree<T> {
     }
     
     pub fn parent(&self) -> Option<Self> {
-        if let Some(node) = self.node.borrow().parent().upgrade() {
+        if let Some(node) = (*self.node).borrow().parent().upgrade() {
             Some(Self { node: node.clone() })
         } else {
             None
@@ -103,57 +103,162 @@ impl<T> Tree<T> {
     }
 
     // ================== Const iterators ==================
-
+    
     pub fn children_iter(&self) -> ChildrenNodesIter<T> {
-        todo!();
+        ChildrenNodesIter::new(self.node.clone())
+    }
+    
+    pub fn parent_iter(&self) -> ParentNodesIter<T> {
+        ParentNodesIter::new(self.node.clone())
     }
 
-    pub fn parent_iter(&self) -> ParentNodesIter<T> {
-        todo!();
+    pub fn nodes_breadth_first_iter(&self) -> NodesBreadthFirstIter<T> {
+        NodesBreadthFirstIter::new(self.node.clone())
     }
+
+    pub fn nodes_depth_first_iter(&self) -> NodesDepthFirstIter<T> {
+        NodesDepthFirstIter::new(self.node.clone())
+    }
+
+    // ================== Const iterators ==================
 
     pub fn items_breadth_first_iter(&self) -> ItemsBreadthFirstIter<T> {
         ItemsBreadthFirstIter::new(self.node.clone())
     }
     
     pub fn items_depth_first_iter(&self) -> ItemsDepthFirstIter<T> {
-        todo!()
-    }
-
-    pub fn nodes_breadth_first_iter(&self) -> NodesBreadthFirstIter<T> {
-        todo!()
-    }
-    
-    pub fn nodes_depth_first_iter(&self) -> NodesDepthFirstIter<T> {
-        todo!()
+        ItemsDepthFirstIter::new(self.node.clone())
     }
 
     // ================== Mutable iterators ==================
-
-    pub fn children_iter_mut(&self) -> ChildrenNodesIterMut<T> {
-        todo!()
-    }
-
-    pub fn parent_iter_mut(&self) -> ParentNodesIterMut<T> {
-        todo!()
-    }
-
+    
     pub fn items_breadth_first_iter_mut(&self) -> ItemsBreadthFirstIterMut<T> {
-        todo!()
+        ItemsBreadthFirstIterMut::new(self.node.clone())
     }
     
     pub fn items_depth_first_iter_mut(&self) -> ItemsDepthFirstIterMut<T> {
-        todo!()
+        ItemsDepthFirstIterMut::new(self.node.clone())
     }
 
-    pub fn nodes_breadth_first_iter_mut(&self) -> NodesBreadthFirstIterMut<T> {
-        todo!()
-    }
-    
-    pub fn nodes_depth_first_iter_mut(&self) -> NodesDepthFirstIterMut<T> {
-        todo!()
-    }
+}
 
+impl<T> Tree<T>
+where T: Display
+{
+    fn format(&self, nesting_level: usize, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for _ in 0..nesting_level {
+            write!(f, "|  ")?;
+        }
+        writeln!(f, "{}", self.get())?;
+        for tree in self.children_iter() { 
+            tree.format(nesting_level+1, f)?
+        }
+        Ok(())
+    }
+}
+
+impl<T> Clone for Tree<T> {
+    fn clone(&self) -> Self {
+        Self { node: self.node.clone() }
+    }
+}
+
+impl<T> Display for Tree<T>
+where T: Display
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.format(0, f)
+    }
+}
+
+pub struct ChildrenNodesIter<'a, T> {
+    list: VecDeque<Tree<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> ChildrenNodesIter<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let list: VecDeque<Tree<T>> = (*root).borrow()
+            .children()
+            .iter()
+            .map(|node| Tree{node: node.clone()})
+            .collect();
+        Self {
+            list,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a, T> Iterator for ChildrenNodesIter<'a, T> {
+    type Item = Tree<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop_front()
+    }
+}
+
+pub struct ParentNodesIter<'a, T> {
+    list: VecDeque<Tree<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> ParentNodesIter<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut list = VecDeque::<Tree<T>>::new();
+        let mut current = (*root).borrow().parent().upgrade();
+        while let Some(parent) = current {
+            current = (*parent).borrow().parent().upgrade();
+            list.push_back(Tree{ node: parent });
+        }
+        Self {
+            list,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<'a, T> Iterator for ParentNodesIter<'a, T> {
+    type Item = Tree<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.list.pop_front()
+    }
+}
+
+pub struct ItemsDepthFirstIter<'a, T> {
+    stack: VecDeque<internal::RcTreeNode<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> ItemsDepthFirstIter<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut this = Self {
+            stack: Default::default(),
+            _marker: PhantomData,
+        };
+        this.stack.push_front(root);
+        this
+    }
+}
+
+impl<'a, T> Iterator for ItemsDepthFirstIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(rc_node) = self.stack.pop_front() {
+            for child in (*rc_node).borrow().children().iter().rev() {
+                self.stack.push_front(child.clone());
+            }
+            let item: *const T = (*rc_node).borrow().get();
+            unsafe {
+                // Need an unbound lifetime to get 'a
+                return item.as_ref();
+            }
+        } else {
+            None
+        }
+    }
 }
 
 pub struct ItemsBreadthFirstIter<'a, T> {
@@ -184,6 +289,146 @@ impl<'a, T> Iterator for ItemsBreadthFirstIter<'a, T> {
             unsafe {
                 // Need an unbound lifetime to get 'a
                 return item.as_ref();
+            }
+        } else {
+            None
+        }
+    }
+}
+
+pub struct NodesDepthFirstIter<'a, T> {
+    stack: VecDeque<Tree<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> NodesDepthFirstIter<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut this = Self {
+            stack: Default::default(),
+            _marker: PhantomData,
+        };
+        this.stack.push_front( Tree{ node: root } );
+        this
+    }
+}
+
+impl<'a, T> Iterator for NodesDepthFirstIter<'a, T> {
+    type Item = Tree<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(tree) = self.stack.pop_front() {
+            let mut list = Vec::<Tree<T>>::new();
+            for child in tree.children_iter() {
+                list.push(child.clone());
+            }
+            for child in list.into_iter().rev() {
+                self.stack.push_front(child);
+            }
+            Some(tree)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct NodesBreadthFirstIter<'a, T> {
+    stack: VecDeque<Tree<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> NodesBreadthFirstIter<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut this = Self {
+            stack: Default::default(),
+            _marker: PhantomData,
+        };
+        this.stack.push_back( Tree{ node: root } );
+        this
+    }
+}
+
+impl<'a, T> Iterator for NodesBreadthFirstIter<'a, T> {
+    type Item = Tree<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(tree) = self.stack.pop_front() {
+            let mut list = Vec::<Tree<T>>::new();
+            for child in tree.children_iter() {
+                list.push(child.clone());
+            }
+            for child in list.into_iter().rev() {
+                self.stack.push_back(child);
+            }
+            Some(tree)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct ItemsDepthFirstIterMut<'a, T> {
+    stack: VecDeque<internal::RcTreeNode<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> ItemsDepthFirstIterMut<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut this = Self {
+            stack: Default::default(),
+            _marker: PhantomData,
+        };
+        this.stack.push_front(root);
+        this
+    }
+}
+
+impl<'a, T> Iterator for ItemsDepthFirstIterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(rc_node) = self.stack.pop_front() {
+            for child in (*rc_node).borrow().children().iter().rev() {
+                self.stack.push_front(child.clone());
+            }
+            let item: *mut T = (*rc_node).borrow_mut().get_mut();
+            unsafe {
+                // Need an unbound lifetime to get 'a
+                return item.as_mut();
+            }
+        } else {
+            None
+        }
+    }
+}
+
+pub struct ItemsBreadthFirstIterMut<'a, T> {
+    stack: VecDeque<internal::RcTreeNode<T>>,
+    _marker: PhantomData<&'a T>,
+}
+
+impl<'a, T> ItemsBreadthFirstIterMut<'a, T> {
+    fn new(root: internal::RcTreeNode<T>) -> Self {
+        let mut this = Self {
+            stack: Default::default(),
+            _marker: PhantomData,
+        };
+        this.stack.push_back(root);
+        this
+    }
+}
+
+impl<'a, T> Iterator for ItemsBreadthFirstIterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(rc_node) = self.stack.pop_front() {
+            for child in (*rc_node).borrow().children() {
+                self.stack.push_back(child.clone());
+            }
+            let item: *mut T = (*rc_node).borrow_mut().get_mut();
+            unsafe {
+                // Need an unbound lifetime to get 'a
+                return item.as_mut();
             }
         } else {
             None
--- tree/src/lib.rs
+++ tree/src/lib.rs
@@ -6,8 +6,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn tree_test() {
+        let mut tree = Tree::new("/");
+        
+        let mut a_dir = Tree::new("a/");
+        let mut b_dir = Tree::new("b/");
+        let mut c_dir = Tree::new("c/");
+
+        let u_file = Tree::new("u: 67");
+        let v_file = Tree::new("v: 1556");
+        let w_file = Tree::new("w: 345");
+        let x_file = Tree::new("x: 6463");
+        let y_file = Tree::new("y: 9621");
+        let z_file = Tree::new("z: 523");
+
+        c_dir.append(z_file);
+
+        b_dir.append(x_file);
+        b_dir.append(y_file);
+
+        a_dir.append(b_dir);
+        a_dir.append(v_file);
+        a_dir.append(w_file);
+
+        tree.append(u_file);
+        tree.append(a_dir);
+        tree.append(c_dir);
+
+        print!("{}", tree);
     }
 }

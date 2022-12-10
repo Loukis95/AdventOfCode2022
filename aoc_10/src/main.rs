9a3ebccbca85f063b4f4e66a1519f01078f5f5d4
Louis Freret <Loukis95@gmail.com>
2022-12-10 09:49:10 +0000

    AoC 10

--- aoc_10/src/main.rs
+++ aoc_10/src/main.rs
@@ -1,8 +1,18 @@
 use std::{env, fs};
 
-enum Values<T> {
-    Skip,
-    Value(T)
+#[derive(Debug, Clone, Copy)]
+struct ProcessorState {
+    cycles: isize,
+    X: isize,
+}
+
+impl ProcessorState {
+    fn new() -> Self {
+        Self {
+            cycles: 0,
+            X: 1,
+        }
+    }
 }
 
 fn main() {
@@ -12,24 +22,45 @@ fn main() {
     let raw_string = String::from_utf8_lossy(&raw_input);
     let input : Vec<_> = raw_string.lines().collect();
 
-    let max_value = input.iter()
-                        .scan(0, |state: &mut usize, &x| {
-                            if x.is_empty() {
-                                let ret = Some(Values::Value(*state));
-                                *state = 0;
-                                ret
-                            } else {
-                                *state += x.parse::<usize>().unwrap();
-                                Some(Values::Skip)
-                            }
-                        })
-                        .filter_map(|x| {
-                            match x {
-                                Values::Skip => None,
-                                Values::Value(v) => Some(v),
-                            }
-                        })
-                        .max().unwrap();
+    let mut state = ProcessorState::new();
 
-    println!("Value: {}", max_value);
+    let sum: isize = input.iter()
+        .filter_map(|line| {
+            let mut ret: Option<isize> = None;
+            let mut it = line.split_whitespace();
+            let instruction = it.next().unwrap();
+            match instruction {
+                "noop" => {
+                    state.cycles += 1;
+
+                    if (state.cycles-20)%40 == 0 {
+                        ret = Some(state.cycles*state.X);
+                        println!("{}: X:{} => {}", state.cycles, state.X, state.cycles*state.X);
+                    }
+                },
+                "addx" => {
+                    state.cycles += 1;
+
+                    if (state.cycles-20)%40 == 0 {
+                        ret = Some(state.cycles*state.X);
+                        println!("{}: X:{} => {}", state.cycles, state.X, state.cycles*state.X);
+                    }
+                    
+                    state.cycles += 1;
+                    
+                    if (state.cycles-20)%40 == 0 {
+                        ret = Some(state.cycles*state.X);
+                        println!("{}: X:{} => {}", state.cycles, state.X, state.cycles*state.X);
+                    }
+                    
+                    state.X += it.next().unwrap().parse::<isize>().unwrap();
+                    
+                },
+                _ => panic!("Unexpected instruction"),
+            }
+            ret
+        })
+        .sum();
+
+    println!("Value: {}", sum);
 }

ac808ff70d716243cb2f730589bf647a4c78d3a4
Louis Freret <Loukis95@gmail.com>
2022-12-13 14:38:34 +0000

    WIP AoC

--- aoc_13/src/main.rs
+++ aoc_13/src/main.rs
@@ -1,12 +1,98 @@
-use std::{env, fs};
+use std::{env, fs, str::FromStr};
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Debug)]
 enum Element {
     Integer(usize),
     List(Vec<Element>),
 }
 
+impl Element {
 
+    #[inline]
+    pub const fn is_integer(&self) -> bool {
+        matches!(*self, Element::Integer(_))
+    }
+
+    #[inline]
+    pub const fn is_list(&self) -> bool {
+        matches!(*self, Element::List(_))
+    }
+}
+
+impl PartialOrd for Element {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        if let Element::Integer(a) = self {
+            if let Element::Integer(b) = other {
+                return a.partial_cmp(b)
+            } else {
+                let mut list = Vec::<Element>::new();
+                list.push(Element::Integer(*a));
+                let list_a = Element::List(list);
+                return list_a.partial_cmp(other)
+            }
+        } else {
+            if let Element::Integer(b) = other {
+                let mut list = Vec::<Element>::new();
+                list.push(Element::Integer(*b));
+                let list_b = Element::List(list);
+                return self.partial_cmp(&list_b)
+            } else {
+                if let Element::List(list_a) = self {
+                    if let Element::List(list_b) = other {
+                        
+                    }
+                    else {
+                        panic!("This should never happen")
+                    }
+                }
+                else {
+                    panic!("This should never happen")
+                }
+            }
+        }
+    }
+}
+
+impl FromStr for Element {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        if let Some(s) = s.strip_prefix("[") {
+            let mut list = Vec::<Element>::new();
+            let mut nesting_level: usize = 0;
+            let mut rest = s;
+            while let Some((idx, _c)) = rest.char_indices().find(|(_idx, c)| {
+                match c {
+                    ']' => if nesting_level == 0 { return true; } else { nesting_level -= 1},
+                    '[' => nesting_level += 1,
+                    ',' => return true,
+                    _ => (),
+                }
+                return false;
+            }) {
+                let (part, tmp) = rest.split_at(idx);
+                rest = tmp.strip_prefix(_c).unwrap();
+                let part = part.trim_end_matches(']');
+                if part.len() != 0 {
+                    if let Ok(element) = part.parse::<Element>() {
+                        list.push(element);
+                    } else {
+                        return Err(());
+                    }
+                }
+            }
+            Ok(Element::List(list))
+        } else {
+            if let Ok(number) = s.parse::<usize>() {
+                return Ok(Element::Integer(number));
+            } else {
+                println!("Error: could not parse \"{}\"", s);
+                return Err(());
+            }
+        }
+    }
+}
 
 fn main() {
     let args : Vec<_> = env::args().collect();
@@ -15,7 +101,16 @@ fn main() {
     let raw_string = String::from_utf8_lossy(&raw_input);
     let input : Vec<_> = raw_string.lines().collect();
 
-    
+    let input_1 = input.iter().step_by(3);
+    let input_2 = input.iter().skip(1).step_by(3);
 
-    println!("answer: {}", path_cost);
+    input_1.zip(input_2).enumerate().for_each(|(n, (a, b))| {
+        let elem_a : Element = a.parse().unwrap();
+        let elem_b : Element = b.parse().unwrap();
+        println!("{}: {:?} <=> {:?} => {:?}", n, a, b, elem_a.partial_cmp(&elem_b));
+    });
+
+    // pairs_iterator.for_each(|(n, (a, b))| {
+    //     println!("{}: {:?} cmp {:?} => {:?}", n, a, b, a.partial_cmp(b));
+    // });
 }

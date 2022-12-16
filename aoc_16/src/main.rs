d987f681fe5268635435ba34b838015a106ac61a
Louis Freret <loukis95@gmail.com>
2022-12-16 08:54:26 +0100

    WIP

--- aoc_16/src/main.rs
+++ aoc_16/src/main.rs
@@ -1,8 +1,65 @@
-use std::{env, fs};
+use std::{env, fs, collections::{HashMap, BinaryHeap, VecDeque}, cmp::Reverse};
 
-enum Values<T> {
-    Skip,
-    Value(T)
+const DEPTH_LIMIT: usize = 30;
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct Valve {
+    name: String,
+    flow_rate: usize,
+    tunnels: Vec<String>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Action {
+    WaitAt(String),
+    MoveToValve(String),
+    OpenValve(String),
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    name: String,
+    open_valves: Vec<String>,
+    action: Action,
+    stack: Vec<Node>,
+    flow_tick: usize,
+    cost: usize,
+    heuristic: usize,
+}
+
+impl PartialEq for Node {
+    fn eq(&self, other: &Self) -> bool {
+        self.heuristic.eq(&other.heuristic)
+    }
+}
+
+impl Eq for Node {}
+
+impl PartialOrd for Node {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.heuristic.partial_cmp(&other.heuristic)
+    }
+}
+
+impl Ord for Node {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.heuristic.cmp(&other.heuristic)
+    }
+}
+
+fn cost(_this: &Node, from: &Node, _graph: &HashMap<String, Valve>) -> usize {
+    from.cost + from.flow_tick
+}
+
+fn heuristic(this: &Node, from: &Node, graph: &HashMap<String, Valve>) -> usize {
+    match &this.action {
+        Action::OpenValve(s) => {
+            let flow_rate = graph.get(s).unwrap().flow_rate;
+            let depth = this.stack.len();
+            from.heuristic + (DEPTH_LIMIT - depth) * flow_rate
+        }
+        _ => from.heuristic
+    }
 }
 
 fn main() {
@@ -12,24 +69,126 @@ fn main() {
     let raw_string = String::from_utf8_lossy(&raw_input);
     let input : Vec<_> = raw_string.lines().collect();
 
-    let max_value = input.iter()
-                        .scan(0, |state: &mut usize, &x| {
-                            if x.is_empty() {
-                                let ret = Some(Values::Value(*state));
-                                *state = 0;
-                                ret
-                            } else {
-                                *state += x.parse::<usize>().unwrap();
-                                Some(Values::Skip)
-                            }
-                        })
-                        .filter_map(|x| {
-                            match x {
-                                Values::Skip => None,
-                                Values::Value(v) => Some(v),
-                            }
-                        })
-                        .max().unwrap();
+    let mut graph = HashMap::<String, Valve>::new();
 
-    println!("Value: {}", max_value);
+    input.iter().for_each(|line| {
+        let mut it = line.split(";");
+        let part1 = it.next().unwrap();
+        let part2 = it.next().unwrap();
+        let mut it = part1.split_whitespace().skip(1);
+        let name = it.next().unwrap();
+        let mut it = it.skip(2);
+        let flow_rate = it.next().unwrap();
+        let flow_rate = flow_rate.split("=").nth(1).unwrap();
+        let flow_rate: usize = flow_rate.parse().unwrap();
+        let mut valve = Valve{name: name.to_string(), flow_rate, tunnels: Vec::<String>::new()};
+        let mut it = part2.split_whitespace().skip(4);
+        while let Some(tunnel) = it.next() {
+            let tunnel = tunnel.trim_start_matches("tunnels leads to valves");
+            let tunnel = tunnel.trim_start_matches("tunnels leads to valve");
+            let tunnel = tunnel.trim_end_matches(",");
+            valve.tunnels.push(tunnel.to_string());
+        }
+        println!("{:?}", valve);
+        graph.insert(name.to_string(), valve);
+    });
+
+    let mut to_visit = BinaryHeap::<Node>::new();
+    let mut found: Option<Node> = None;
+
+    let start = Node { name: "AA".to_string(), open_valves: Vec::<String>::new(), action: Action::WaitAt("AA".to_string()), stack: Vec::<Node>::new(), flow_tick: 0, cost: 0, heuristic: 0 };
+    to_visit.push(start);
+    while found.is_none() {
+        let current = to_visit.pop().unwrap();
+        {
+            println!("Exploring node with depth: {}, cost: {}, h: {}", current.stack.len(), current.cost, current.heuristic);
+            // for (n, step) in current.stack.iter().enumerate() {
+            //     println!("======== Step {} ========", n);
+            //     match &step.action {
+            //         Action::WaitAt(s) => println!("Chill at node {}", s),
+            //         Action::OpenValve(s) => println!("Open valve {}", s),
+            //         Action::MoveToValve(s) => println!("Move to valve {}", s),
+            //     }
+            //     println!("Released pressure: {}", step.cost);
+            // }
+            // println!("");
+        }
+        if current.stack.len() >= DEPTH_LIMIT {
+            found = Some(current);
+            continue;
+        }
+        // Try to open the valve if not already open
+        if !current.open_valves.contains(&current.name) {
+            let mut open_valves = current.open_valves.clone();
+            open_valves.push(current.name.clone());
+            let mut stack = current.stack.clone();
+            stack.push(current.clone());
+            let flow_tick = current.flow_tick + graph.get(&current.name).unwrap().flow_rate;
+            let mut next = Node { 
+                name: current.name.clone(),
+                open_valves,
+                action: Action::OpenValve(current.name.clone()),
+                stack: stack, 
+                flow_tick,
+                cost: 0,
+                heuristic: 0
+            };
+            let cost = cost(&next, &current, &graph);
+            let heuristic = heuristic(&next, &current, &graph);
+            next.cost = cost;
+            next.heuristic = heuristic;
+            // Add nodes to the list
+            to_visit.push(next);
+        }
+        // Move to nearby valves
+        let nearby_valves = &graph.get(&current.name).unwrap().tunnels;
+        for nearby_valve in nearby_valves {
+            let mut stack = current.stack.clone();
+            stack.push(current.clone());
+            let mut next = Node { 
+                name: nearby_valve.clone(),
+                open_valves: current.open_valves.clone(),
+                action: Action::MoveToValve(nearby_valve.clone()),
+                stack: stack, 
+                flow_tick: current.flow_tick,
+                cost: 0,
+                heuristic: 0
+            };
+            let cost = cost(&next, &current, &graph);
+            let heuristic = heuristic(&next, &current, &graph);
+            next.cost = cost;
+            next.heuristic = heuristic;
+            // Add nodes to the list
+            to_visit.push(next);
+        }
+        // Or we can just chill
+        // let mut stack = current.stack.clone();
+        // stack.push(current.clone());
+        // let mut next = Node { 
+        //     name: current.name.clone(),
+        //     open_valves: current.open_valves.clone(),
+        //     action: Action::WaitAt(current.name.clone()),
+        //     stack: stack, 
+        //     flow_tick: current.flow_tick,
+        //     cost: 0,
+        //     heuristic: 0
+        // };
+        // let cost = cost(&next, &current, &graph);
+        // let heuristic = heuristic(&next, &current, &graph);
+        // next.cost = cost;
+        // next.heuristic = heuristic;
+        // // Add nodes to the list
+        // to_visit.push(next);
+    }
+
+    let node = found.unwrap();
+    for (n, step) in node.stack.iter().enumerate() {
+        println!("======== Step {} ========", n);
+        match &step.action {
+            Action::WaitAt(s) => println!("Chill at node {}", s),
+            Action::OpenValve(s) => println!("Open valve {}", s),
+            Action::MoveToValve(s) => println!("Move to valve {}", s),
+        }
+        println!("Released pressure: {}", step.cost);
+    }
 }

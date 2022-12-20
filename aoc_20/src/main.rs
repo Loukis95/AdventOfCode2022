c1f3abd562d896738ae1cd80a1bf217ee04822b5
Louis Freret <Loukis95@gmail.com>
2022-12-20 15:04:50 +0100

    Update main.rs

--- aoc_20/src/main.rs
+++ aoc_20/src/main.rs
@@ -1,35 +1,104 @@
-use std::{env, fs};
+struct Link<T> {
+    item: T,
+    next: Option<Rc<RefCell<T>>>,
+    previous: Option<Rc<RefCell<T>>>,
+    origin_next: Option<Rc<RefCell<T>>>,
+    origin_previous: Option<Rc<RefCell<T>>>,
+    this: Option<Rc<RefCell<T>>>,
+}
 
-enum Values<T> {
-    Skip,
-    Value(T)
+impl<T> Link<T> {
+    fn new(item: T) -> Self {
+        Self {
+            item,
+            next: None,
+            previous: None,
+            origin_next: None,
+            origin_previous: None,
+            this: None,
+        }
+    }
+    
+    fn insert_before(&self, item: T) {
+        let previous_link = self.previous.clone();
+        let this = self.this.clone();
+        let link = Self::new(item);
+        link.next = Some(this.clone());
+        link.previous = Some(previous_link.clone());
+        link.origin_next = Some(this.clone());
+        link.origin_previous = Some(previous_link.clone());
+        let rc_link = Rc::new(RefCell::new(link));
+        let link_clone = rc_link.clone();
+        let borrowed_link = (*rc_link).borrow_mut();
+        borrowed_link.this = Some(link_clone.clone());
+        let borrowed_prev = (*previous_link).borrow_mut();
+        borrowed_prev.next = Some(link_clone.clone());
+        borrowed_prev.origin_next = Some(link_clone.clone());
+        this.previous = Some(link_clone.clone());
+        this.origin_previous = Some(link_clone);
+    }
+    
+    fn insert_after(&self, item: T) {
+        let next_link = self.next.clone();
+        let this = self.this.clone();
+        let link = Self::new(item);
+        link.next = Some(next_link.clone());
+        link.previous = Some(this.clone());
+        link.origin_next = Some(next_link.clone());
+        link.origin_previous = Some(this.clone());
+        let rc_link = Rc::new(RefCell::new(link));
+        let link_clone = rc_link.clone();
+        let borrowed_link = (*rc_link).borrow_mut();
+        borrowed_link.this = Some(link_clone.clone());
+        let borrowed_next = (*next_link).borrow_mut();
+        borrowed_prev.previous = Some(link_clone.clone());
+        borrowed_prev.origin_previous = Some(link_clone.clone());
+        this.next = Some(link_clone.clone());
+        this.origin_next = Some(link_clone);
+    }
+}
+
+struct LinkedList<T> {
+    head: Option<Link<T>>,
+    len: usize,
+}
+
+impl<T> LinkedList<T> {
+    fn new() -> Self {
+        Self {
+            head: None,
+            len: 0,
+        }
+    }
+    
+    fn push_back(&mut self, item: T) {
+        if self.len == 0 {
+            self.insert_first_item(item);
+        } else {
+            (*self.head).borrow_mut().insert_before(item);
+            self.len += 1;
+        }
+    }
+    
+    fn insert_first_item(&mut self, item: T) {
+        let link = Link::new(item);
+        let rc_link = Rc::new(RefCell::new(link));
+        let link_clone = rc_link.clone();
+        let borrowed_link = (*rc_link).borrow_mut();
+        borrowed_link.next = Some(link_clone.clone());
+        borrowed_link.previous = Some(link_clone.clone());
+        borrowed_link.origin_next = Some(link_clone.clone());
+        borrowed_link.origin_previous = Some(link_clone.clone());
+        borrowed_link.this = Some(link_clone);
+        self.head = Some(rc_link);
+        self.len = 1;
+    }
+}
+
+struct LinkedListOriginIterator<'a, T> {
+    
 }
 
 fn main() {
-    let args : Vec<_> = env::args().collect();
-    let input_path = &args[1];
-    let raw_input = fs::read(input_path).unwrap();
-    let raw_string = String::from_utf8_lossy(&raw_input);
-    let input : Vec<_> = raw_string.lines().collect();
-
-    let max_value = input.iter()
-                        .scan(0, |state: &mut usize, &x| {
-                            if x.is_empty() {
-                                let ret = Some(Values::Value(*state));
-                                *state = 0;
-                                ret
-                            } else {
-                                *state += x.parse::<usize>().unwrap();
-                                Some(Values::Skip)
-                            }
-                        })
-                        .filter_map(|x| {
-                            match x {
-                                Values::Skip => None,
-                                Values::Value(v) => Some(v),
-                            }
-                        })
-                        .max().unwrap();
-
-    println!("Value: {}", max_value);
+    println!("Hello, world!");
 }
